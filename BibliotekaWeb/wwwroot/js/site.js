d4ad647d644079ff497ad0380847239d33868ffa
Marcin <lisowskim@gmail.com>
2020-06-21 21:09:30 +0200

    Dodanie podglądu ładowanego zdjęcia

--- BibliotekaWeb/wwwroot/js/site.js
+++ BibliotekaWeb/wwwroot/js/site.js
@@ -2,3 +2,33 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function previewFiles() {
+
+    var preview = document.querySelector('#preview');
+    var files = document.querySelector('input[type=file]').files;
+
+    function readAndPreview(file) {
+
+        // Make sure `file.name` matches our extensions criteria
+        if (/\.(jpe?g|png|gif)$/i.test(file.name)) {
+            var reader = new FileReader();
+            preview.innerText = ''
+            reader.addEventListener("load", function () {
+                var image = new Image();
+                image.height = 100;
+                image.title = file.name;
+                image.src = this.result;
+                preview.appendChild(image);
+            }, false);
+
+            reader.readAsDataURL(file);
+        }
+
+    }
+
+    if (files) {
+        [].forEach.call(files, readAndPreview);
+    }
+
+}
